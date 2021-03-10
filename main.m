4589e26d8f80ab1c9af3bd57928bb5230fc015a5
erikschroter <erikschroter@gmail.com>
2021-03-10 16:04:04 +0100

    fixing Anton's wonderful error :-)

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
 
 %la vision porobe has 756 hz
 
-%snapshotList = importSnapshotList("Case0.dat", "DATAPACKING", "ZONE");
+snapshotList = importSnapshotList("Case0.dat", "DATAPACKING", "ZONE");
 %snapshotList1 = importSnapshotList("Case1.dat", "DATAPACKING", "ZONE");
 %snapshotList2 = importSnapshotList("Case2.dat", "DATAPACKING", "ZONE");
 
-plotSnapshotList(snapshotList1, 1/756)
+plotSnapshotList(snapshotList, 1/756)
 %plotCombinedSnapshotList(snapshotList1, snapshotList2, 1/756)
--- plotSnapshotList.m
+++ plotSnapshotList.m
@@ -1,17 +1,18 @@
-function plotSnapshotList(snapshotList, delay)
+function plotSnapshotList(snapshotList1, delay)
 
 colormap(jet);
 colorbar;
     
-for i = 1:length(snapshotList)
-    pause(delay)subplot(1,2,1)
-    scatter3(snapshotList1{i}.xmm, snapshotList1{i}.ymm, snapshotList1{i}.zmm, 5, snapshotList{i}.I)
+for i = 1:length(snapshotList1)
+    pause(delay)
+    subplot(1,2,1)
+    scatter3(snapshotList1{i}.xmm, snapshotList1{i}.ymm, snapshotList1{i}.zmm)
     title('Isometric', int2str(i))
     axis equal
     set(gca,'XLim',[-800 -400],'YLim',[500 700],'ZLim',[550 950])
     
     subplot(1,2,2)
-    scatter3(snapshotList1{i}.xmm, snapshotList1{i}.ymm, snapshotList1{i}.zmm, 5, snapshotList{i}.I)
+    scatter3(snapshotList1{i}.xmm, snapshotList1{i}.ymm, snapshotList1{i}.zmm)
     view(0,90)
     title('top')
     axis equal
