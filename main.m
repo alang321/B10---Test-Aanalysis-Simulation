65d815bcf5f742348507aec6ca913b52ae18da69
Tiia <tiia.tikkala@gmail.com>
2021-03-12 15:28:19 +0100

    Added folder for outlier detection stuff

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 
 %la vision porobe has 756 hz
 
-snapshotList = importSnapshotList("Case0.dat", "DATAPACKING", "ZONE");
+%snapshotList = importSnapshotList("Case0.dat", "DATAPACKING", "ZONE");
 %snapshotList1 = importSnapshotList("Case1.dat", "DATAPACKING", "ZONE");
 %snapshotList2 = importSnapshotList("Case2.dat", "DATAPACKING", "ZONE");
 
