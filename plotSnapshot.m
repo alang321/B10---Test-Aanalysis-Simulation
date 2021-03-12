c39c83cd19b685bc95db02fd37c72e8319cab840
andonius <34992832+alang321@users.noreply.github.com>
2021-03-12 16:39:49 +0100

    implemented cluster sorting, and a function to plot it
     (it works atleast for case0)

--- plotSnapshot.m
+++ plotSnapshot.m
@@ -1,5 +1,29 @@
-function plotSnapshot(snapshot)
-
-scatter3(snapshot.xmm, snapshot.ymm, snapshot.zmm)
+function plotSnapshotList(snapshotList, delay)
+    
+for i = 1:length(snapshotList)
+    subplot(2,2,1)
+    scatter3(snapshotList{i}.xmm, snapshotList{i}.ymm, snapshotList{i}.zmm)
+    title('Isometric', int2str(i))
+    axis equal
+    set(gca,'XLim',[-800 -400],'YLim',[500 700],'ZLim',[550 950])
+    
+    subplot(2,2,2)
+    scatter3(snapshotList{i}.xmm, snapshotList{i}.ymm, snapshotList{i}.zmm)
+    view(0,90)
+    title('top')
+    axis equal
+    set(gca,'XLim',[-800 -400],'YLim',[500 700],'ZLim',[550 950])
+    title('Top', int2str(i))
+    
+    subplot(2,2,3)
+    scatter3(snapshotList{i}.xmm, snapshotList{i}.ymm, snapshotList{i}.zmm)
+    view(90,0)
+    title('top')
+    axis equal
+    set(gca,'XLim',[-800 -400],'YLim',[500 700],'ZLim',[550 950])
+    title('Front', int2str(i))
+    
+    pause(delay)
+end
 
 end
