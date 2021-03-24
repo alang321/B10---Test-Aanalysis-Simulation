7a8c3106f8fc9893f8f14ea3e88ad7f3d373541c
Tiia <t.e.e.tikkala@student.tudelft.nl>
2021-03-24 17:17:38 +0100

    Added clarifying comments to SVD method

--- SVDRotation.m
+++ SVDRotation.m
@@ -1,9 +1,12 @@
 function [R, T] = SVDRotation(d, m)
 % This function takes in two point sets, d and m, and calculates the
 % rotation and transformation matrices, R and T, between the sets as per
-% Eggert et al. (1997)
+% Eggert et al. (1997): "Estimating 3-D rigid body transformations: a 
+% comparison of four major algorithms"
 
 % Check the paper for the derivation of these equations
+% When actually implementing this, check the size of d and m, maybe can't
+% use length()?
 dbar = 1/length(d) * sum(d);
 mbar = 1/length(m) * sum(m);
 
